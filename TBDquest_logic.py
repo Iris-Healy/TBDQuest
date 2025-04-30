decf428938eb96316d526f558bba680e98a7eb87
Iris <kl3224zr@go.minnstate.edu>
2025-04-30 10:48:09 -0500

    Second commit fixing exit game issues

--- TBDquest_logic.py
+++ TBDquest_logic.py
@@ -50,6 +50,7 @@ class Room:
         self.adjacents.append(adjacent_room)
 
     def get_adjacent_rooms(self):
+        '''getter for accessing adjacent rooms'''
         return self.adjacents
 
 class Player:
@@ -60,37 +61,48 @@ class Player:
         self.inventory.add(hand)
 
     def input_command(self):
-        ''''''
+        '''handles inputting command stripping player input and making it lowercase'''
         command = input(">").strip().lower()
         self.process_command(command)
 
     def process_command(self, command):
+        '''Processing for commands'''
         if command == "take":
             item_name = input(f"Which item do you wish to take\n >")
             self.take_item(item_name)
 
         elif command == "check inventory":
+            #check inv returns all items in your inventory
             print("Your inventory contains:")
             for item in self.inventory:
                 print(item.name)
 
         elif command == "examine item":
+            #calls examine_item function
             examine_target = input(f"What would you like to examine\n >")
             self.examine_item(examine_target)
 
         elif command == "examine room":
+            #prints the description of the room the player is in
             print(self.current_room.description)
 
         elif command == "use":
+            #calls the use_item function taking player input for the key and lock targets
             key_target = input("Which item would you like to use\n >")
             lock_target = input("Which item would you like to use it on\n >")
             self.use_item(key_target,lock_target)
 
         elif command == "go":
+            #calls the go function taking user input as the go target
             go_target = input("Where would you like to go? \n >")
             self.travel_to(go_target)
 
+        elif command == "exit":
+            #calls the get gamestate setter to set playing to false exiting the loop
+            Game.set_gamestate(game,False)
+
         elif command == "help":
+            #lists commands for the player
             print(f"commands are listed as followed\n"
                   f" go - travel to new room \n"
                   f" take - appends item to your inventory \n"
@@ -103,7 +115,9 @@ class Player:
                   f" exit - exits the game")
 
     def take_item(self, item_name):
+        '''take item function takes input from player in '''
         i_found = False
+        #loops through items in current room searching for an item name that matches player com
         for item in self.current_room.room_items:
             if item.name == item_name and item.carry == True:
                 i_found = True
@@ -115,9 +129,6 @@ class Player:
             print(f"You pick up the {item_name}")
         if not i_found:
             print(f"{item_name} is not in the room")
-
-    def navigate(self, adjacent_room_name):
-        self.current_room = self.current_room.adjacents[adjacent_room_name]
 
     def examine_item(self, examine_target):
         e_item = None
@@ -134,13 +145,13 @@ class Player:
 
     def travel_to(self, go_target):
         g_room = None
-        for room in Room.get_adjacent_rooms():
+        for room in Room.get_adjacent_rooms(self.current_room):
             if room.name == go_target:
                 g_room = room
 
         if g_room is not None:
             Player().current_room = g_room
-            print(f"You walk to {g_room.name}")
+            print(f"You walk to the {g_room.name}")
         else:
             print(f"{go_target} can not be traveled to")
 
@@ -182,12 +193,22 @@ class Game:
     def __init__(self):
         self.player = Player()
         self.current_room = start_room
+        self.playing = True
+
+    def get_gamestate(self):
+        return self.playing
+
+    def set_gamestate(self,x:bool):
+        self.playing = x
 
     def start_game(self):
         print("you awake in a strange room you don't remember anything")
-        while True:
+        while self.playing:
             self.player.input_command()
             self.player.process_command(self)
+            if not self.playing:
+                print(f"Thank you for playing!")
+                break
 
 
 
@@ -195,7 +216,7 @@ start_room = Room(1, "start room", "The room is very plain there is a door to th
                                            "Above the door there is a sign reading Test Chamber 1"
                                            " there is a pumpkin on the floor in the center of the room."
                                            " to the west there is a console on the wall", )
-test_room1 = Room(2,"test room","this is a test room")
+test_room1 = Room(2,"Test Chamber 1","this is a test room")
 hand = KeyItem(0,"hand",True,"your hand",0)
 pumpkin = Item(1, "pumpkin", True, "what pumpkin? there is a shiny copper key on the floor")
 console = Item(2, "console", False, "its stuck to the wall", )
